026428ff96879c6477e4171b7d34480f942938ae
AhmetMosby <ahmettulgay@gmail.com>
2024-04-04 20:56:21 +0300

    pushAhmet

---
+++ Git/Controllers/AntiqueWhite1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git.Controllers
+{
+    public class AntiqueWhite1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
