f142a67bcf63300f6c2a4d9c40ba5eeb186fe861
emrecevahir <emreckorkmaz@gmail.com>
2024-04-04 21:01:06 +0300

    AntiqueWhite3Controller eklendi

---
+++ Git/Controllers/AntiqueWhite3Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git.Controllers
+{
+    public class AntiqueWhite3Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
