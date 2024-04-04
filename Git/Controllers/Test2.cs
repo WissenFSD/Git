942a18bba96c7388a112fcd319adbdef81c18f08
sinanozlen03 <sinanozlen3@gmail.com>
2024-04-04 21:34:58 +0300

    WissenCommentAdded

---
+++ Git/Controllers/Test2.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git.Controllers
+{
+    public class Test2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Git/Controllers/WissenBauControllers.cs
+++ Git/Controllers/WissenBauControllers.cs
@@ -2,6 +2,6 @@
 {
 	public class WissenBauControllers
 	{
-
+		//Wissen Controller
 	}
 }
